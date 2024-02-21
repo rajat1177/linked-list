901132031909e21dab1856759f61008ade0ebdef
Rajat Kumar <rajat.kumargzb1177@gmail.com>
2024-02-21 19:06:55 +0530

    done

--- insert.cpp
+++ insert.cpp
@@ -36,79 +36,87 @@ void travesal(node* &head){
         temp=temp->link;
     }
 }
-node* deletebeg(node*head){
-    if(head==nullptr) return head;
-    node*temp=head;
-    head=temp->link;
-    delete temp;
+node* insertbeg(node*head,int value){
+    node* temp=new node(value);
+    temp->link=head;
+    head=temp;
     return head;
 }
-node* deletetail(node*head){
-    if(head==nullptr||head->link==nullptr) return nullptr; //2 conditions
-    node*temp=head;
-    while(temp->link->link){
-        temp=temp->link; 
+node* inserttail(node*head,int value){
+    node* newnode=new node(value);
+    node* temp=head;
+    while(temp->link){
+        temp=temp->link;
     }
-    temp->link=nullptr;
-    temp=temp->link;
-    delete temp;
+    temp->link=newnode;
     return head;
 }
-node* deletefromkthposition(node*head,int k){
-    if(head==nullptr) return head;
+
+node* insertatk(node*head,int value,int k){
+    
+    if(head==nullptr){
+        if(k==1){
+            return new node(value);
+        }
+        else{
+            return nullptr;
+        }
+    }
     if(k==1){
-        node*temp=head;
-        head=temp->link;
-        delete temp;
+        node* temp=new node(value);
+        temp->link=head;
+        head=temp;
         return head;
     }
     int count=0;
+    node* newnode=new node(value);
     node* temp=head;
-    node* prev=nullptr;
     while(temp){
         count++;
-        if(count==k){
-            prev->link=temp->link;
+        if(count==k-1){
+            newnode->link=temp->link;
+            temp->link=newnode;
             break;
+
         }
-        prev=temp;
-        temp=temp->link; 
+        temp=temp->link;
     }
-    delete temp;
     return head;
 }
-node* deletebyvalue(node*head,int value){
-    if(head==nullptr) return head;
-    if(head->data==value){
-        node*temp=head;
-        head=temp->link;
-        delete temp;
+node* insertBeforeElement(node*head,int value,int element){
+    
+    
+    if(head->data==element){
+        node* temp=new node(value);
+        temp->link=head;
+        head=temp;
         return head;
     }
     
+    node* newnode=new node(value);
     node* temp=head;
-    node* prev=nullptr;
     while(temp){
         
-        if(temp->data==value){
-            prev->link=temp->link;
+        if(temp->link->data==element){
+            newnode->link=temp->link;
+            temp->link=newnode;
             break;
+
         }
-        prev=temp;
-        temp=temp->link; 
+        temp=temp->link;
     }
-    delete temp;
     return head;
 }
+
 
 
 int main() {
     vector<int> v={1,2,3,4,5};
     node* head=convertarr2LL(v);
-    // head=deletebeg(head); 
-    // head=deletetail(head); 
-    // head=deletefromkthposition(head,2); 
-    // head=deletebyvalue(head,5);
+    // head=insertbeg(head,0);
+    // head=inserttail(head,6);
+    // head=insertatk(head,50,2);
+    head=insertBeforeElement(head,0,5);
     travesal(head);
     return 0;
 }
